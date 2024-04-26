a235b2a62fd436beae6dcfceb385692cf5716099
pauliusla <pauldarkas@gmail.com>
2024-04-26 18:55:27 +0200

    feat(user): add default policy config for users
    Added default policy configuration to better control the authorization who will be able to view different pages

--- back-end-api/app/Http/Controllers/UserController.php
+++ back-end-api/app/Http/Controllers/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Validation\ValidationException;
 
@@ -16,6 +17,8 @@ class UserController extends Controller
      */
     public function index()
     {
+        $this->authorize("view-users", Auth::user());
+
         return response([
             // "users" => User::with("courses")->latest()->paginate(10)
             "users" => User::paginate(10)
