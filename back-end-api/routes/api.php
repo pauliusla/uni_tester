bfea9f637a75900cb02d62aab26193101e572847
pauliusla <pauldarkas@gmail.com>
2024-04-29 11:00:21 +0200

    feat(user): add missing routes
    Added missing routes for setting user to admin and updating password

--- back-end-api/routes/api.php
+++ back-end-api/routes/api.php
@@ -5,6 +5,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [UserController::class, 'store']);
 Route::post('/login', [UserController::class, 'login']);
+Route::PUT('/users/set-admin/{user}', [UserController::class, 'setAdmin'])
+    ->middleware('auth:sanctum');
+Route::PUT('/users/update-password/{user}', [UserController::class, 'updateUserPassword'])
+    ->middleware('auth:sanctum');
 
 Route::resource('users', UserController::class)
     ->only(['index', "show", "destroy", "update"])
