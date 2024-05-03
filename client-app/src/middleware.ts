3f831ebcc09910aaa5068a6735314ee0a9e0fd91
pauliusla <pauldarkas@gmail.com>
2024-05-03 14:02:39 +0200

    feat:
     block login and register routes if user exists

--- client-app/src/middleware.ts
+++ client-app/src/middleware.ts
@@ -1,10 +1,14 @@
 import { getToken } from "next-auth/jwt";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { authRoutes, publicRoutes } from "./config/routesConfig";
 
-// This function can be marked `async` if using `await` inside
 export async function middleware(req: NextRequest) {
   const session = await getToken({ req });
+
+  if (authRoutes.includes(req.nextUrl.pathname) && session) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
 
   return NextResponse.next();
 }
